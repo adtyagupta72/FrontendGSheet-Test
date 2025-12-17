3bfdc16cc115815494c8093dfb4faf7d25cf7360
Aditya Gupta <adtyagupta72@gmail.com>
2025-12-17 21:10:13 +0530

    trying to resolve corse issue

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-let submitted = false;
-  function onSuccess() {
-    alert("Form submitted successfully!");
-    submitted = false;
+    
+
+    document
+    .getElementById("loadDataBtn")
+    .addEventListener("click", () => {
+      fetch("/.netlify/functions/getSheetData")
+        .then(res => res.json())
+        .then(data => {
+          renderTable(data);
+        })
+        .catch(err => {
+          console.error(err);
+          alert("Failed to load data");
+        });
+    });
+
+  function renderTable(data) {
+    if (!data.length) {
+      document.getElementById("sheetData").innerHTML =
+        "<p>No data found</p>";
+      return;
+    }
+
+    let table = "<table border='1'><tr>";
+
+    Object.keys(data[0]).forEach(key => {
+      table += `<th>${key}</th>`;
+    });
+
+    table += "</tr>";
+
+    data.forEach(row => {
+      table += "<tr>";
+      Object.values(row).forEach(val => {
+        table += `<td>${val}</td>`;
+      });
+      table += "</tr>";
+    });
+
+    table += "</table>";
+
+    document.getElementById("sheetData").innerHTML = table;
   }
